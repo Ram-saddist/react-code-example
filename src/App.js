c6506c5b7fa178e098e48c65522ff4c294e2edae
Ram-saddist <nagulavancharavisivaramteja@gmail.com>
2023-02-26 20:41:36 +0530

    added msg in App.js file

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import MessageList from './components/MessageList'
 function App() {
   return (
     <div className="App">
+      <h1>This is from app.jscls</h1>
       <MessageList/>
     </div>
   );
